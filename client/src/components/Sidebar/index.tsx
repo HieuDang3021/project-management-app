72ddc675eec66137d05b185ee074be3c9c5d772b
HieuDang3021 <trhieu1247@gmail.com>
2025-01-08 23:41:12 +1100

    navbar and sidebar setup

---
+++ client/src/components/Sidebar/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+import { LockIcon } from 'lucide-react';
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+const Sidebar = () => {
+  // const [showProjects, setShowProjects] = useState(true);
+  // const [showPriority, setShowPriority] = useState(true);
+
+  const sidebarClassNames = 'fixed flex flex-col h-full justify-between shadow-xl transition-all duration-300 z-30 bg-white dark:bg-black dark:text-white overflow-y-auto w-64';
+
+  return (
+    <div className={sidebarClassNames}>
+      <div className='flex h-[100%] w-full flex-col justify-start'>
+        {/* TOP TITLE */}
+        <div className='z-50 flex  min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black'>
+          <div className='text-xl font-bold text-gray-800 dark:text-white'>
+            Projects
+          </div>
+        </div>
+        {/* TEAM */}
+        <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700'>
+          <Image src={'/logo.png'} alt='logo' width={40} height={40}/>
+          <div>
+            <h3 className='text-base font-bold tracking-wide dark:text-gray-200'>
+              MY TEAM
+            </h3>
+            <div className='mt-1 flex items-start gap-2'>
+              <LockIcon className='size-3 dark:text-gray-400 mt-[0.1rem] text-gray-500'/>
+              <p className='text-xs text-gray-500 dark:text-gray-400'>Private</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +9,35 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: '#ffffff',
+        gray: {
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          500: '#6b7280',
+          700: '#374151',
+          800: '#1f2937',
+        },
+        blue: {
+          200: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+        },
+        'dark-bg': '#111827',
+        'dark-bg-2': '#1f2937',
+        'dark-bg-3': '#374151',
+        'blue-primary': '#3b82f6',
+        'stroke-dark': '#1f2937',
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
       },
     },
   },
