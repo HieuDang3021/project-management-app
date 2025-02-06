d09db4c11d7dbfdcde9c3e7682823b3788da1d6c
HieuDang3021 <trhieu1247@gmail.com>
2025-02-06 21:14:34 +1100

    ecosystem for EC2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+ module.expors = {
+  app: [
+    {
+      name: "project-management",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+      }
+    }
+  ]
+ }
--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
 /* SERVER */
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 4000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
